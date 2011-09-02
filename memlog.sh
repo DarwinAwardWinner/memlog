6b7b3ff09702f7f95130f5eca7d0829f0c2111ae
Ryan C. Thompson <rct@thompsonclan.org>
2011-09-01 21:59:27 -0400

    add memlog.sh

---
+++ memlog.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+PID="$1"
+
+# Returns true/false (i.e. 0/1)
+process_exists () {
+  kill -0 "$1" 2>/dev/null
+}
+
+print_process_memory () {
+  /bin/echo -e "$(pmap $$ | tail -n1 | awk '{print $2}')\t$(date)"
+}
+
+while process_exists $PID; do
+  print_process_memory $PID
+  sleep 5
+done
